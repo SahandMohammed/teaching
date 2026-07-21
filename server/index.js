70c7aaaa53736cbabd6c6f8a654ae1b353ec03b4
SahandMohammed <snqiu210234@uniq.edu.iq>
2026-07-21 10:06:00 +0300

    Refactor code structure for improved readability and maintainability

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.json({
+    message: "Hello API!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
